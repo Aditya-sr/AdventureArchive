a7de90181844c33a3d6f8b1d3c0f8b72d1209bd6
Aditya <aadityasrivastava99@gmail.com>
2026-01-31 15:47:21 +0530

    Upadyed Code

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -5,8 +5,12 @@
     @if ($openModal)
         <div class="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-70 z-50 overflow-y-auto">
             <div class="w-2/4 h-3/4 relative">
-                <x-card> <h1>kya hal hai londe.</h1></x-card>
-              
+                <x-card> <h1>kya hal hai londe.
+                paojfpoaosfjpas
+                aojfoafoas
+                akjsbsfaljsfbjoasjfaofs
+                </h1></x-card>
+
             </div>
         </div>
     @endif
